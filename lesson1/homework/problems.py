5e9495ab858713bae38c9c35a4baaa3a12470d10
Hamzaseawa <Hamza.seawa@gmail.com>
2026-02-20 21:48:47 -0800

    homework 1 completed

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,32 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+X = 7
+print(X)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
+Y = 9
+print(Y)
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
+W = "watermelon"
+print("i like" ,W )
 
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
+s = "seatlle"
+u = "Amareca"
+print(s)
+print(u)     
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+p = "spike"
+i = 6
+t = "dog"
+print("my pet",t,"is",i,"years old and his name is",p)
